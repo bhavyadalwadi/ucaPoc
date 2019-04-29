90448c86e57d8993a7be351b4a64f562e578a7af
Bhavya Ashokkumar <37308562+basho00@users.noreply.github.com>
2019-04-28 18:20:19 -0700

    adding header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,24 @@
-function loadDoc() {
-    console.log('fdsfsf');
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
+function _loadHeader() {
+    console.log('Header Loaded');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("cncAEMHeader").innerHTML = this.responseText;
+            // console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "https://www.tomthumb.com/content/experience-fragments/www/tomthumb/en/generic_content/header/header.content.html", true);
+    xhttp.send();
+}
+function _loadFooter() {
+    console.log('Footer Loaded');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("cncAEMFooter").innerHTML = this.responseText;
+            // console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "https://www.tomthumb.com/content/experience-fragments/www/tomthumb/en/generic_content/footer/footer.content.html", true);
+    xhttp.send();
 }
